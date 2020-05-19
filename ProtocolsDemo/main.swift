2147607a744b1c6cebe44ffd622e355aa0045d18
Nicole Liu <nicoleliu_@outlook.com>
2020-05-19 00:29:13 -0400

    Initial Commit

---
+++ ProtocolsDemo/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ProtocolsDemo
+//
+//  Created by Liu, Bojun on 2020-05-19.
+//  Copyright © 2020 Liu, Nicole. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
